68f99d5b070dce866600b6630b8a68d3a235bf27
Brian <brian.dickman@gmail.com>
2011-07-29 19:10:28 -0700

    Strip Pandora's special artist properties like (Children's) and (Holiday)

--- content_script.js
+++ content_script.js
@@ -1,9 +1,8 @@
-
 (function(){
     var titles = $('table#tbl_thumbs_up').find('span.track_title');
     var data = [];
     for (var i = 0; titles.length > i; i++) {
-        var artist = $(titles[i]).closest('tr').find('a[title="Artist details"]').text();
+        var artist = $(titles[i]).closest('tr').find('a[title="Artist details"]').text().split("(")[0];
         data.push({title:$(titles[i]).attr('tracktitle'), artist:artist});
     }
     chrome.extension.sendRequest(data);
