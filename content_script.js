99dbd698a7895a654916bc93b5d72319f91fe24c
Brian <brian.dickman@gmail.com>
2011-07-29 22:16:44 -0700

    Also include "Song Seeds" in track list, remove duplicates.

--- content_script.js
+++ content_script.js
@@ -1,9 +1,12 @@
 (function(){
-    var titles = $('table#tbl_thumbs_up').find('span.track_title');
+    var titles = $('table#tbl_thumbs_up,table#tbl_track_bookmarks').find('span.track_title');
     var data = [];
     for (var i = 0; titles.length > i; i++) {
         var artist = $(titles[i]).closest('tr').find('a[title="Artist details"]').text().split("(")[0];
-        data.push({title:$(titles[i]).attr('tracktitle'), artist:artist});
+        var track = {title:$(titles[i]).attr('tracktitle'), artist:artist};
+        if(!data.some(function(element, index, array){
+            return(element['artist'] == track['artist'] && element['title'] == track['title']); }))
+            data.push(track);
     }
     chrome.extension.sendRequest(data);
 })();
