32c25c750c435c32a3eabd31895873bbbab7207e
Nobu Funaki <nobu@zuzara.com>
2011-09-21 10:05:17 -0700

    supports new pandora ui

--- content_script.js
+++ content_script.js
@@ -1,9 +1,9 @@
 (function(){
-    var titles = $('table#tbl_thumbs_up,table#tbl_track_bookmarks').find('span.track_title');
+    var titles = $('ul.thumb_up_list,ul.song_list').find('li');
     var data = [];
     for (var i = 0; titles.length > i; i++) {
-        var artist = $(titles[i]).closest('tr').find('a[title="Artist details"]').text().split("(")[0];
-        var track = {title:$(titles[i]).attr('tracktitle'), artist:artist};
+        var artist = $(titles[i]).data('artist').split("(")[0];
+        var track = {title:$(titles[i]).find('h3 a').text(), artist:artist};
         if(!data.some(function(element, index, array){
             return(element['artist'] == track['artist'] && element['title'] == track['title']); }))
             data.push(track);
